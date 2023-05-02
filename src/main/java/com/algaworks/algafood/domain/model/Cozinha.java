f8f857e5aa027675a22fca376dc432ae8b4b39a6
Daniel Marques <danielvictorsm@gmail.com>
2023-05-02 20:34:03 -0300

    feat(modulo3): Criando entidades Cozinha e Restaurante

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Cozinha {
+    @Id
+    private Long id;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return id.equals(cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
